23e1c0d78072df3808b3892d1f2220c3b9d3741e
v2krishna <v2krishna@gmail.com>
2019-12-16 05:40:50 -0600

    Added my code first app

---
+++ apps/01_hello_world/you_try/01_hello_world.py
@@ -0,0 +1,8 @@
+print("--------------------------")
+print("       Hello World        ")
+print("--------------------------")
+
+
+name = input("Whats your name ?")
+print("Nice to meet you {}".format(name))
+
